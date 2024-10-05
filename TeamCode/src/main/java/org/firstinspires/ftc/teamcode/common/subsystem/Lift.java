b67bca5cf6ca61399ef79612bfdd7edf483f31ca
Crympty <CrimsonCrympty@gmail.com>
2024-10-05 13:48:29 -0700

    pedro pathing file added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Lift.java
@@ -21,7 +21,7 @@ public class Lift implements Subsystem {
 
 
     TICKS_PER_REV = 384.5, // ticks
-            PULLEY_CIRCUMFERENCE = 4.40945, // inches
+            PULLEY_CIRCUMFERENCE = 4.40945;// inches
 
     public static int
             MIN_POS = 0, MAX_POS = 2500,
@@ -44,7 +44,7 @@ public class Lift implements Subsystem {
     double targetVelocity;
     double currentVelocity;
 
-    public Lift(HardwareMap hardwareMap) {
+    public Lift(HardwareMap hardwareMap, DcMotorEx motor) {
 
         this.robot = RobotHardware.getInstance();
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Teleop.java
@@ -1,16 +1,23 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-public class Teleop extends OpMode  {
+
+@TeleOp
+public class Teleop extends CommandOpMode {
 
     @Override
-    public void init() {
+    public void initialize() {
+        CommandScheduler.getInstance().reset();
+
 
     }
 
     @Override
-    public void loop() {
+    public void run() {
 
     }
 }
