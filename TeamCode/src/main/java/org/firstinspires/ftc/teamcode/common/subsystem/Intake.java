9022b3da3390910ee034d61b2be6ff1b80b0c556
james <597829@cjuhsd.net>
2024-10-23 18:38:25 -0700

    converted intake to new action library init and start are not done, depends on how we do the thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Intake.java
@@ -40,7 +40,7 @@ public class Intake {
         twistArm = new RunAction(this::twistArm);
 
     }
-    //o
+    //ok
     //spin to win part
     public void setSpin(State state, boolean onlyChangeState) {
         if (onlyChangeState) {
