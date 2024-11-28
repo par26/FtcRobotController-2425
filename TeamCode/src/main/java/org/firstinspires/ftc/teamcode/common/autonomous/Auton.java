0aed665bca950861859b92fb43b2cb57a27b7415
samxielaptop <crimsoncrympty@gmail.com>
2024-11-27 17:47:23 -0800

    bucket buildpath done, rest wip

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/Auton.java
@@ -4,6 +4,7 @@ import static org.firstinspires.ftc.teamcode.common.autonomous.FieldConstants.*;
 
 import org.firstinspires.ftc.teamcode.common.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.common.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.common.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.common.pedroPathing.pathGeneration.BezierLine;
 import org.firstinspires.ftc.teamcode.common.pedroPathing.pathGeneration.Path;
 import org.firstinspires.ftc.teamcode.common.pedroPathing.pathGeneration.PathChain;
@@ -16,15 +17,19 @@ public class Auton {
 
     public Follower follower;
 
-    public Path element1, score1, element2, score2, element3, score3;
-    public PathChain pushSamples, depositPreload, specimen1, specimen2, specimen3, grab1, grab2, grab3, park;
+    //public Path element1, score1, element2, score2, element3, score3;
+    //public PathChain pushSamples, depositPreload, specimen1, specimen2, specimen3, grab1, grab2, grab3, park;
+    //Paths & Pathchains
+    public Path depositPreload, sample1, score1, sample2, score2, sample3, score3;
+    public PathChain pushSamples;
+    //Poses
     //Both
     public Pose spawnPose, preloadPose, parkPose;
     //Bucket
     //TODO: we gon need vision for bucket
     public Pose sample1Control, sample1Pose, sample2Pose, sample3Pose, scorePose;
     //Observation
-    public Pose grabPose, specimen1Pose, specimen2Pose, specimen3Pose, specimen4Pose;
+    public Pose grabPose, specimen1Pose, specimen2Pose, specimen3Pose, specimen4Pose, specimen5Pose;
 
 
     public Auton(RobotStart startLocation) {
@@ -58,6 +63,11 @@ public class Auton {
                 preloadPose = blueObservationPreloadPose;
                 grabPose = blueObservationGrabPose;
                 parkPose = blueObservationParkPose;
+                specimen1Pose = blueSpecimen1Pose;
+                specimen2Pose = blueSpecimen2Pose;
+                specimen3Pose = blueSpecimen3Pose;
+                specimen4Pose = blueSpecimen4Pose;
+                specimen5Pose = blueSpecimen5Pose;
 
                 break;
             case RED_BUCKET:
@@ -76,6 +86,11 @@ public class Auton {
                 preloadPose = redObservationPreloadPose;
                 grabPose = redObservationGrabPose;
                 parkPose = redObservationParkPose;
+                specimen1Pose = redSpecimen1Pose;
+                specimen2Pose = redSpecimen2Pose;
+                specimen3Pose = redSpecimen3Pose;
+                specimen4Pose = redSpecimen4Pose;
+                specimen5Pose = redSpecimen5Pose;
 
                 break;
         }
@@ -85,18 +100,36 @@ public class Auton {
 
     public void buildPaths() {
         if (startLocation == RobotStart.RED_BUCKET || startLocation == RobotStart.BLUE_BUCKET) {
-            depositPreload = follower.pathBuilder()
-                    .addPath(new BezierLine(new Point(spawnPose), new Point(preloadPose)))
-                    .setLinearHeadingInterpolation(spawnPose.getHeading(), preloadPose.getHeading())
-                    .build();
+            depositPreload = new Path(new BezierLine(new Point(spawnPose), new Point(preloadPose)));
+            depositPreload.setLinearHeadingInterpolation(spawnPose.getHeading(), preloadPose.getHeading());
+
+            sample1 = new Path(new BezierCurve(new Point(preloadPose), new Point(sample1Control), new Point(sample1Pose)));
+            sample1.setLinearHeadingInterpolation(preloadPose.getHeading(), sample1Pose.getHeading());
+
+            score1 = new Path(new BezierLine(new Point(sample1Pose), new Point(scorePose)));
+            score1.setLinearHeadingInterpolation(sample1Pose.getHeading(), scorePose.getHeading());
+
+            sample2 = new Path(new BezierLine(new Point(scorePose), new Point(sample2Pose)));
+            sample2.setLinearHeadingInterpolation(scorePose.getHeading(), sample2Pose.getHeading());
+
+            score2 = new Path(new BezierLine(new Point(sample2Pose), new Point(scorePose)));
+            score2.setLinearHeadingInterpolation(sample2Pose.getHeading(), scorePose.getHeading());
+
+            sample3 = new Path(new BezierLine(new Point(scorePose), new Point(sample3Pose)));
+            sample3.setLinearHeadingInterpolation(scorePose.getHeading(), sample3Pose.getHeading());
+
+            score3 = new Path(new BezierLine(new Point(sample3Pose), new Point(scorePose)));
+            score3.setLinearHeadingInterpolation(sample3Pose.getHeading(), scorePose.getHeading());
 
 
         }
 
         if (startLocation == RobotStart.RED_OBSERVATION || startLocation == RobotStart.BLUE_OBSERVATION) {
-            depositPreload = follower.pathBuilder()
-                    .addPath(new BezierLine(new Point(spawnPose), new Point(preloadPose)))
-                    .setLinearHeadingInterpolation(spawnPose.getHeading(), preloadPose.getHeading())
+            depositPreload = new Path(new BezierLine(new Point(spawnPose), new Point(preloadPose)));
+            depositPreload.setLinearHeadingInterpolation(spawnPose.getHeading(), preloadPose.getHeading());
+
+            pushSamples = follower.pathBuilder()
+                    .addPath(new BezierCurve())
                     .build();
         }
     }
