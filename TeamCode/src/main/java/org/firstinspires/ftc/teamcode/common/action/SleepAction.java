1721aa3fcc7e99bf7e2216791f713d08adc8620c
rocketmind26 <apratush26@gmail.com>
2025-01-26 21:33:07 -0800

    fixed sleep action bc i'm like that

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/action/SleepAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/action/SleepAction.java
@@ -5,27 +5,29 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 public class SleepAction implements Action {
     private final double dt;
     private boolean isFinished = false;
+    private ElapsedTime timer;
 
-    public SleepAction(double dt) {this.dt = dt;
+    public SleepAction(double dt) {
+        this.dt = dt;
+        this.timer = new ElapsedTime();
     }
 
     @Override
     public boolean run(TelemetryPacket p) {
         if (!isFinished) {
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    isFinished = true;
-                }
-            }, (long) (dt * 1000));
-        } else {
-            isFinished = false; // Reset the flag after sleep is complete
-            return false; // Indicate that the action is finished
+            if (timer.seconds() >= dt) {
+                isFinished = true;
+                return false;
+            }
+            return true;
         }
-        return true; // Indicate that the action is still running
+        return false;
     }
 }
